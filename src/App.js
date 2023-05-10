cddb0cbd66ed8fecc4427cde0dd3bec0f5f6e238
adil <adilmovies7@gmail.com>
2023-05-10 10:31:08 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import Login from './Login';
-import Signup from './Signup';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Login from "./Login";
+import Signup from "./Signup";
 function App() {
-  const [current,setCurrent] = useState(0);
+  const [current, setCurrent] = useState(0);
+  function switchSignUp() {
+    setCurrent(1)
+  }
+  function switchLogIn() {
+    setCurrent(0)
+  }
   return (
     <div className="App">
-        <div className='container'>
-
-            <div className='login-sign-up-option'>
-              <div onClick={()=>{
-                setCurrent(0);
-                let option1 = document.querySelector('.login-option');
-                let option2 = document.querySelector('.sign-up-option');
-                if(option1.classList.contains('current'))
-                {
-                  return;
-                }
-                option1.classList.toggle('current') 
-                option2.classList.toggle('current') 
-              }} className='login-option option current'>
-                Log In
-              </div>
-              <div onClick={()=>{
-                setCurrent(1);
-                let option1 = document.querySelector('.login-option');
-                let option2 = document.querySelector('.sign-up-option');
-                if(option2.classList.contains('current'))
-                {
-                  return;
-                }
-                option1.classList.toggle('current') 
-                option2.classList.toggle('current') 
-              }} className='sign-up-option option'>
-                Sign Up
-              </div>
-            </div>
-            {current===0?<Login current={current}setCurrent={setCurrent}/>:<Signup current={current}setCurrent={setCurrent}/>}
+      <div className="container">
+        <div className="login-sign-up-option">
+          <div
+            onClick={() => {
+              let option1 = document.querySelector(".login-option");
+              let option2 = document.querySelector(".sign-up-option");
+              if (option1.classList.contains("current")) {
+                return;
+              }
+              let loginForm = document.querySelector(".sign-up-form");
+              loginForm.style.marginRight = "-1000px";
+              option1.classList.toggle("current");
+              option2.classList.toggle("current");
+              setTimeout(switchLogIn,230)
+            }}
+            className="login-option option current"
+          >
+            Log In
+          </div>
+          <div
+            onClick={() => {
+              let option1 = document.querySelector(".login-option");
+              let option2 = document.querySelector(".sign-up-option");
+              if (option2.classList.contains("current")) {
+                return;
+              }
+              let loginForm = document.querySelector(".login-form");
+              loginForm.style.marginLeft = "-1000px";
+              option1.classList.toggle("current");
+              option2.classList.toggle("current");
+              setTimeout(switchSignUp,230)
+            }}
+            className="sign-up-option option"
+          >
+            Sign Up
+          </div>
         </div>
+        {current === 0 ? (
+          <Login current={current} setCurrent={setCurrent} />
+        ) : (
+          <Signup current={current} setCurrent={setCurrent} />
+        )}
+      </div>
     </div>
   );
 }
--- src/Signup.js
+++ src/Signup.js
@@ -2,23 +2,23 @@ import React from 'react'
 import './Signup.css'
 const Signup = (current, setCurrent) => {
   return (
-    <div className='sign-up-form'>
+    <form className='sign-up-form'>
         <input className='input-pet' placeholder='First Name' type='text' required/>
         <input className='input-pet' placeholder='Last Name' type='text' required/>
         <input className='input-pet' placeholder='Email Id' type='email' required/>
         <input className='input-pet' placeholder='Password' type='password' required/>
         <input className='input-pet' placeholder='Confirm Password' type='password' required/>
         <div className='agree'>
-            <input type='checkbox' />
+            <input type='checkbox' required/>
             <div className='agree-text'>
                 I agree to the terms and conditions
             </div>
         </div>
-        <input placeholder='Password' type='submit' value='Sign Up' />
+        <input type='submit' value='Sign Up' />
         <div className='info'>
             Already have an account? Log In
         </div>
-    </div>
+    </form>
   )
 }
 
